1ef756b1b586da7070f4b7d7e747e53cafe2bd50
mohan <mohan14242@gmail.com>
2024-08-09 11:45:18 +0530

    mohan

--- 1.sh
+++ 1.sh
@@ -3,3 +3,9 @@
 
 read -p "software to install" 
 echo "the os $(uname)"
+
+if [ "$(uname)" = "Linux" ]; then 
+    echo "installing in the linuc" 
+else
+    echo "installing on the another operating system " 
+fi
