9ab50fbef6f99ab65f3fc0aa1b176681a9501be1
mohan <mohan14242@gmail.com>
2024-08-15 10:08:58 +0530

    mohan

--- 2.sh
+++ 2.sh
@@ -4,3 +4,6 @@ echo "this is the program get first 10 bigest files in file system passed via po
 path="$1"
 echo $path
 du -ah $path | sort -hr  | head -n 10 >filesize.txt 
+
+### herer we are getting biggest files inside the specified path  
+
---
+++ 3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+if [ $# -eq o ]; then 
+    echo "no arguments are providec" 
+fi
