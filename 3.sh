038a0946e949a55cb6e52119404caae1b4c8fbe8
mohan <mohan14242@gmail.com>
2024-08-15 10:19:01 +0530

    mohan

--- 3.sh
+++ 3.sh
@@ -2,4 +2,12 @@
 
 if [[ $# -eq o ]]; then 
     echo "no arguments are providec" 
+    exit 1
+fi
+
+## now check the wheathe user is root user or not 
+
+if [[ $(id -u) -ne 0 ]]; then 
+    echo "please run this command as root user" 
+    exit 2
 fi
