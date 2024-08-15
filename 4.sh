fa12534382b8b71d450db95442d9f13a0bb1d323
mohan <mohan14242@gmail.com>
2024-08-15 10:59:39 +0530

    mohan

--- 4.sh
+++ 4.sh
@@ -5,7 +5,7 @@ packages="$@"
 for pack in $packages 
 do 
     if systemctl is-active --quiet $pack ; then 
-        echo "$pack is started" 
+        echo " it is already $pack is started" 
     else 
         echo "it is not started" 
         sudo systemctl start $pack
